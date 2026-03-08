76f721ffa1b27b098d808f031d8636c96630d49e
Amit Bhaskar <amit.bhaskar@gmail.com>
2026-03-09 12:55:00 +1300

    Delete list_models.py
    File deleted.

--- list_models.py
+++
@@ -1,7 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AQ.Ab8RN6KU3pcGyKGAKoBDkKnkjvRciCD_o1lsFhBnP208JTPinA")
-
-for m in client.models.list():
-    if "gemini" in m.name.lower():
-        print(m.name)
